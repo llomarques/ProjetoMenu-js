a68ba2b58cddc66a6b1d8fc75e7c75bc52595730
unknown <dev@DSN1191348087.edu119.local>
2026-02-05 11:14:34 -0300

    05/02

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,132 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.ArrayList;
+import java.util.ArrayList;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    private static ArrayList<Aluno> listaAluno = new ArrayList<>();
+    private static  ArrayList<Turma> listaTurma= new ArrayList<>();
+    public static void main(String[] args) {
+        menuPrincipal();
+        }
+
+    public static void menuPrincipal(){
+        System.out.println("=== Secretaria ===");
+        System.out.println("1 - Alunos");
+        System.out.println("2 - Turmas");
+        System.out.println("3 - Sair");
+        String opcao = Leitura.dados("Digite a opção desejada:");
+
+        switch (opcao){
+            case "1":
+               menuAlunos();
+            case "2":
+              menuTurmas();
+            case "3":
+                System.out.println("Até a próxima 😘✌️");
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Opção inválida! Tente novamente 🤔");
+
+        }
+
+    }
+
+    private static void menuTurmas() {
+        System.out.println("==== Turmas ====");
+        System.out.println("1 - Listar Turmas");
+        System.out.println("2 - Cadastrar Turma");
+        System.out.println("3 - Atualizar Turma");
+        System.out.println("4 - Excluir turma");
+        System.out.println("5 - Voltar ao menu principal");
+        String opcao = Leitura.dados("Digite a opção desejada:");
+        switch (opcao){
+            case "1":
+                listarTurmas();
+                break;
+            case "2":
+                cadastrarTurma();
+                break;
+            case "3":
+                atualizarTurma();
+                break;
+            case "4":
+                excluirTurma();
+                break;
+            case "5":
+                menuPrincipal();
+                break;
+            default:
+                System.out.println("Opção inválida! Tente novamente 🤔");
+
         }
     }
+
+    private static void menuAlunos() {
+        System.out.println("==== Aluno ====");
+        System.out.println("1 - Listar Alunos");
+        System.out.println("2 - Cadastrar Aluno");
+        System.out.println("3 - Atualizar Aluno");
+        System.out.println("4 - Excluir Aluno");
+        System.out.println("5 - Voltar ao menu principal");
+        String opcao = Leitura.dados("Digite a opção desejada:");
+        switch (opcao){
+            case "1":
+                listarAlunos();
+                break;
+            case "2":
+                cadastrarAluno();
+                break;
+            case "3":
+                atualizarAluno();
+                break;
+            case "4":
+                excluirAluno();
+                break;
+            case "5":
+                menuPrincipal();
+                break;
+            default:
+                System.out.println("Opção inválida! Tente novamente 🤔");
+
+        }
+
+    }
+
+    private static void excluirAluno() {
+    }
+
+    private static void atualizarAluno() {
+        
+    }
+
+    private static void cadastrarAluno() {
+        
+    }
+
+    private static void listarAlunos() {
+        for (Aluno a: listaAluno){
+            System.out.println(a);
+        }
+        
+    }
+
+    private static void excluirTurma() {
+        
+    }
+
+    private static void atualizarTurma() {
+        
+    }
+
+    private static void cadastrarTurma() {
+        
+    }
+
+    private static void listarTurmas() {
+        for (Turma t: listaTurma){
+            System.out.println(t);
+        }
+        
+    }
+
 }
